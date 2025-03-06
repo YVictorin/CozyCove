bde5d3d431cb43190ade0604c85eee54916ca939
YVictorin <vicyonjou@gmail.com>
2025-03-06 18:13:37 -0500

    feat(client): initialize project with basic pages and components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,24 @@
-import './global.css'
+import { Routes, Route } from "react-router-dom";
+import Layout from "./components/Layout";
+
+import Home from "./pages/Home"
+import NotFound from "./pages/NotFound";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Account from "./pages/Account"
 
 function App() {
   return (
     <>
-      <div>Home Page</div>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="register" element={<Register />} />
+        <Route path="login" element={<Login />} />
+        <Route path="account" element={<Account />} />
+        <Route path="*" element={<NotFound />} />
+      </Route>
+    </Routes>
     </>
   )
 }
