3563580d2d3849493581a5c79841ec51c7234348
Itaty <itatyconstante@gmail.com>
2025-03-11 15:08:53 -0400

    adding the path for pages

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -5,9 +5,10 @@ export default function Navbar() {
         <div>
             <ul>
                 <li><Link to="/">Home</Link></li>
-                <li><Link to="/register">Register</Link></li>
+                <li><Link to="/games">Games</Link></li>
+                <li><Link to="Products">Products</Link></li>
                 <li><Link to="/login">Login</Link></li>
-                <li><Link to="/account">Account</Link></li>
+                <li><Link to="/explore">Explore More</Link></li>
             </ul>
         </div>
     )
