ffea6c8ae4fd609c522607d2eb4b1f171a31d2ac
sergio7quintero <yuyisq@gmail.com>
2025-03-11 15:22:50 -0400

    pulled updated pages

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,14 +1,22 @@
 import { Link } from "react-router-dom";
 
 export default function Navbar() {
-    return (
-        <div>
-            <ul>
-                <li><Link to="/">Home</Link></li>
-                <li><Link to="/register">Register</Link></li>
-                <li><Link to="/login">Login</Link></li>
-                <li><Link to="/account">Account</Link></li>
-            </ul>
-        </div>
-    )
+  return (
+    <div>
+      <ul className="header">
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/register">Register</Link>
+        </li>
+        <li>
+          <Link to="/login">Login</Link>
+        </li>
+        <li>
+          <Link to="/account">Account</Link>
+        </li>
+      </ul>
+    </div>
+  );
 }
