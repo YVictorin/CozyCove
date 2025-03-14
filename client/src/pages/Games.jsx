f5ced9f7ad8e79f489d4b7d9aaaabfc2aba26ebe
DungTran <curindn921@gmail.com>
2025-03-14 15:47:09 -0400

    Added game

--- client/src/pages/Games.jsx
+++ client/src/pages/Games.jsx
@@ -1,26 +1,29 @@
 import { useState } from "react";
+import { Link } from "react-router-dom";
 import GameCard from "../components/GameCard";
 import Game from "../components/game/MainGame";
 
 export default function GamesPage() {
-    const [showGame, setShowGame] = useState(false);
+  const [showGame, setShowGame] = useState(false);
 
-    const handleRoutineGameClick = () => {
-        setShowGame(true); 
-    };
+  // Cozy Routines
+  const handleRoutineGameClick = () => {
+    setShowGame(true);
+  };
 
-    return (
-        <div className="w-screen h-screen flex flex-col items-center justify-center gap-6">
-            <h2 className="text-4xl font-bold">ONLINE GAMES</h2>
-
-            {showGame ? (
-                <Game />
-            ) : (
-                <div className="flex gap-6">
-                    <GameCard onClick={handleRoutineGameClick} title="Cozy Routines" />
-                    <GameCard />
-                </div>
-            )}
+  return (
+    <div className="w-screen h-screen flex flex-col items-center justify-center gap-6">
+      <h2 className="text-4xl font-bold">ONLINE GAMES</h2>
+      {showGame ? (
+        <Game />
+      ) : (
+        <div className="flex gap-6">
+          <GameCard onClick={handleRoutineGameClick} title="Cozy Routines" />
+          <Link to="/games/emotions-artist">
+            <GameCard title="Emotions Artist" />
+          </Link>
         </div>
-    );
+      )}
+    </div>
+  );
 }
