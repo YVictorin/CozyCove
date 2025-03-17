d55887fd1d7694a27d36e80d8299b16768326c5d
YVictorin <vicyonjou@gmail.com>
2025-03-17 13:38:47 -0400

    refactor: update allowedOrigins to include hosted url

--- server/src/config/security/allowedOrigins.js
+++ server/src/config/security/allowedOrigins.js
@@ -16,9 +16,15 @@
 
     'https://cozycove.vercel.app/',
     'https://cozycove.vercel.app/api',
+    'https://cozycove.vercel.app/api/register',
+    'https://cozycove.vercel.app/api/login',
 
     'https://cozycove-git-yvpages-yonjous-projects.vercel.app/register',
-    'https://cozycove-git-yvpages-yonjous-projects.vercel.app/'
+    'https://cozycove-git-yvpages-yonjous-projects.vercel.app/login',
+    'https://cozycove-git-yvpages-yonjous-projects.vercel.app/',
+    'https://cozycove-git-yvpages-yonjous-projects.vercel.app/api/register',
+    'https://cozycove-git-yvpages-yonjous-projects.vercel.app/api/login',
+
 
 ]
 
