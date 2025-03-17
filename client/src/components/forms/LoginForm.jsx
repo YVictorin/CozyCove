3b625ad616b6150e09f42c02681bdf9a53b1e25e
YVictorin <vicyonjou@gmail.com>
2025-03-17 16:57:02 -0400

    refactor: update fetch url for deployment

--- client/src/components/forms/LoginForm.jsx
+++ client/src/components/forms/LoginForm.jsx
@@ -23,7 +23,7 @@ function LoginForm() {
 
   const onSubmit = async (data) => {
     try {
-     const response = await fetch('http://localhost:3001/api/login', {
+     const response = await fetch('https://cozycove-server-git-yvpages-yonjous-projects.vercel.app/api/login', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         credentials: 'include',  
