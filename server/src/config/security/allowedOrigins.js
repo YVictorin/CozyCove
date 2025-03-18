85618e80021a38f5e8c0417d5e5d047f3d74f888
DungTran <curindn921@gmail.com>
2025-03-18 19:31:35 -0400

    Resolved CORS and JWT

--- server/src/config/security/allowedOrigins.js
+++ server/src/config/security/allowedOrigins.js
@@ -1,6 +1,7 @@
 //feel free to add any urls you need to fetch to and from to the list
  const allowedOrigins = [
     'http://localhost:3000',
+    'http://localhost:3001',
 
     'http://127.0.0.1:5173',
     'http://127.0.0.1:5174',
@@ -9,11 +10,13 @@
     'http://localhost:5173',
     'http://localhost:5174',
     'http://localhost:5175',
+    'http://localhost:5173',
 
     'http://localhost:5173/login',
     'http://localhost:5174/login',
     'http://localhost:5175/login',
 
-]
 
-export default allowedOrigins
+];
+
+export default allowedOrigins;
