6b324968ce105b0268cae117a676d74c79eb5604
Itaty <itatyconstante@gmail.com>
2025-03-21 13:55:25 -0400

    fixing responsiveness

--- client/src/components/GameCard.jsx
+++ client/src/components/GameCard.jsx
@@ -1,30 +1,29 @@
-
 export default function GameCard({ title = "Unknown Title", imageUrl }) {
-  return (
-    <div className="w-full md:w-[40rem] h-[22rem] md:h-[30rem] rounded-xl overflow-hidden
-      transition-transform duration-300 hover:scale-105 cursor-pointer"
-    >
-      <div
-        className="w-full h-[60%] md:h-[70%]"
-        style={{
-          backgroundImage: `url(${imageUrl})`,
-          backgroundSize: 'cover',
-          backgroundPosition: 'center',
-        }}
-      ></div>
-      <div className="w-full h-[40%] md:h-[30%] bg-white">
-        <div className="p-2 md:p-4">
-          <div
-            className="w-24 md:w-30 flex justify-center content-center rounded-sm text-xs md:text-sm bg-[rgba(119,224,199,0.38)] py-1 px-2 md:py-[0.225rem] mb-1 md:mb-[0.25rem]"
-            style={{
-              fontFamily: "'Roboto', sans-serif",
-            }}
-          >
-            Online Games
+    return (
+      <div className="w-full md:w-[40rem] h-[22rem] md:h-[30rem] rounded-xl overflow-hidden
+        transition-transform duration-300 hover:scale-105 cursor-pointer"
+      >
+        <div
+          className="w-full h-[70%]"
+          style={{
+            backgroundImage: `url(${imageUrl})`,
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+          }}
+        ></div>
+        <div className="w-full h-[30%] bg-white">
+          <div className="p-2 md:p-4">
+            <div 
+              className="w-24 md:w-30 flex justify-center content-center rounded-sm text-xs md:text-sm bg-[rgba(119,224,199,0.38)] py-1 px-2 md:py-[0.225rem] mb-1 md:mb-[0.25rem]"
+              style={{
+                fontFamily: "'Roboto', sans-serif",
+              }}
+            >
+              Online Games
+            </div>
+            <div className="text-xl md:text-3xl font-extrabold">{title}</div>
           </div>
-          <div className="text-xl md:text-3xl font-extrabold">{title}</div>
         </div>
       </div>
-    </div>
-  );
-}
+    );
+  }
