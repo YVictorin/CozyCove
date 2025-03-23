6a87073d25dd5f1e990669c51fba28ea57dd6d68
YVictorin <vicyonjou@gmail.com>
2025-03-23 12:06:14 -0400

    refactor: improve responsiveness for register form page

--- client/src/components/forms/MultiStepForm.jsx
+++ client/src/components/forms/MultiStepForm.jsx
@@ -133,8 +133,8 @@ const MultiStepForm = () => {
       {success && <div className="mb-4 text-green-500">{success}</div>}
 
       {/* Step Indicator */}
-      <div className="w-full  mt-16 max-w-3xl mb-4 px-4 flex items-center justify-between">
-        <div>
+      <div className={`w-full mt-24 max-w-3xl mb-4 px-4 flex items-center justify-between`}>
+      <div>
           {currentStep > 1 ? (
             <button onClick={handlePrevStep} className="mr-4 text-gray-500 hover:text-gray-700">
               <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
