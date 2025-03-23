8830a7bd03b507eb5cdd0829b3b528de3bba1915
YVictorin <vicyonjou@gmail.com>
2025-03-23 12:49:13 -0400

    refactor(account): update responsiveness display

--- client/src/components/game/tasks/morning/BrushTeethGame.jsx
+++ client/src/components/game/tasks/morning/BrushTeethGame.jsx
@@ -79,7 +79,7 @@ export default function BrushTeethGame({ onCompleteTask }) {
   return (
     <div className="relative w-full h-full" tabIndex={0} onKeyDown={handleKeyDown} ref={gameAreaRef}>
       <div className="absolute text-center w-full bottom-2 text-blue-600 text-sm">
-        Use WASD keys to move and catch the toothbrush!
+        Click anywhere and use WSAD to move around
       </div>
 
       {/* Character */}
